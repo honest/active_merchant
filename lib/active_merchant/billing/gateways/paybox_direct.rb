4281c831d338093a3ae71e199f98003624bbd4e3
aizotovhonest <aizotov@honest.com>
2015-09-14 10:32:33 -0700

    TECH-104 remove deprecated iconv

--- lib/active_merchant/billing/gateways/paybox_direct.rb
+++ lib/active_merchant/billing/gateways/paybox_direct.rb
@@ -1,5 +1,3 @@
-require 'iconv'
-
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class PayboxDirectGateway < Gateway
@@ -132,7 +130,8 @@ module ActiveMerchant #:nodoc:
       end
 
       def parse(body)
-        body = Iconv.iconv("UTF-8","LATIN1", body.to_s).join
+        body = body.to_s.encode('UTF-8', 'LATIN1', :invalid => :replace, :undef => :replace, :replace => '?').join
+
         results = {}
         body.split(/&/).each do |pair|
           key,val = pair.split(/\=/)
